68dd28f08c722b48de5fe103b382844938c0066a
lyuwalle <lyuwalle@163.com>
2022-04-05 20:25:06 +0800

    fix

--- src/main/java/com/lyuwalle/backend/Repo/JobLevelRepo.java
+++ src/main/java/com/lyuwalle/backend/Repo/JobLevelRepo.java
@@ -10,6 +10,7 @@ import com.lyuwalle.backend.utils.BeanCopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,6 +27,7 @@ public class JobLevelRepo {
     }
 
     public int addJobLevel(JobLevel jobLevel) {
+        jobLevel.setCreateDate(new Date());
         return jobLevelDbMapper.insert(BeanCopyUtil.copy(jobLevel, JobLevelDB.class));
     }
 
